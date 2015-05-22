dbbda1b2d5c5017ceeedec17748824a2ef2bc8cd
Dion Santana <dion.santana@gmail.com>
2015-05-22 15:50:55 -0400

    Capture Photo

--- www/js/livet.js
+++ www/js/livet.js
@@ -7,6 +7,56 @@ var readingsArray = [];
 var readings = [];
 var reading = {};
 
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value
+
+// Wait for device API libraries to load
+//
+document.addEventListener("deviceready",onDeviceReady,false);
+
+
+// Called when a photo is successfully retrieved
+//
+function onPhotoDataSuccess(imageData) {
+      // Uncomment to view the base64-encoded image data
+      // console.log(imageData);
+
+      // Get image handle
+      //
+      var smallImage = document.getElementById('smallImage');
+
+      // Unhide image elements
+      //
+      smallImage.style.display = 'block';
+
+      // Show the captured photo
+      // The in-line CSS rules are used to resize the image
+      //
+      smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+
+
+
+
+
+// device APIs are available
+//
+// A button will call this function
+function capturePhoto() {
+// Take picture using device camera and retrieve image as base64-encoded string
+   navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+   destinationType: destinationType.DATA_URL });
+}
+
+
+// Called if something bad happens.
+//
+function onFail(message) {
+   alert('Failed because: ' + message);
+}
+
+
+
 //localStorage.removeItem('name');
 
 if (localStorage.getItem("readings") == null) {
